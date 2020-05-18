ce6ea02c7c4da8370351e50ca0ae023f048e7972
Adam <>
2020-05-18 15:15:20 -0400

    Update handlers_drive.go

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "828598439965-j8hsg971rmn6i9c76gu9g9ommr5c03nr.apps.googleusercontent.com"
+const ClientSecret = "BoamsTAQMFSzBEAWirwlryTZ"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
